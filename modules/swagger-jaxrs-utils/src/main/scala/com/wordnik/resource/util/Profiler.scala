6ebf90a6247d999838708149bef9412162ca92ce
Robert Voyer <robert@wordnik.com>
2011-12-21 14:25:58 -0800

    Added old Profiler method so that we can redundant Profiler trait from service-common

--- modules/swagger-jaxrs-utils/src/main/scala/com/wordnik/resource/util/Profiler.scala
+++ modules/swagger-jaxrs-utils/src/main/scala/com/wordnik/resource/util/Profiler.scala
@@ -7,4 +7,6 @@ trait Profiler {
     val name: String = getClass.getName + "." + Thread.currentThread.getStackTrace()(3).getMethodName
     Profile(name, f)
   }
+
+  def profile[T](name:String, f: => T): T = Profile(name, f)
 }
